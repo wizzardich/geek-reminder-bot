b607f7f0fa19bd42a7cad6715f5a0c0a294b1629
Vlad Tomashpolskyi <wizzardich@gmail.com>
2020-06-20 00:12:01 +0200

    Improve environment configuration

--- main.go
+++ main.go
@@ -11,14 +11,39 @@ import (
 var hostEmail string
 var hostTimeZone string
 
+const tokenEnv = "GO_TELEGRAM_TOKEN"
+const emailEnv = "GO_DOODLE_EMAIL"
+const timezEnv = "GO_DOODLE_TZ"
+const localEnv = "GO_EXTERNAL_ADDRESS"
+
 func main() {
-	token := os.Getenv("GO_TELEGRAM_TOKEN")
+	token := os.Getenv(tokenEnv)
 
 	if token == "" {
-		log.Fatalln("Environment variable GO_TELEGRAM_TOKEN is not defined.")
+		log.Fatalf("Environment variable %s is not defined.\n", tokenEnv)
+	}
+
+	hostEmail = os.Getenv(emailEnv)
+
+	if hostEmail == "" {
+		log.Fatalf("Environment variable %s is not defined.\n", emailEnv)
+	}
+
+	localURL := os.Getenv(localEnv)
+
+	if hostEmail == "" {
+		log.Fatalf("Environment variable %s is not defined.\n", localEnv)
+	}
+
+	hostTimeZone = os.Getenv(timezEnv)
+
+	if hostTimeZone == "" {
+		log.Printf("Environment variable %s is not defined, using UTC timezone.\n", timezEnv)
+		hostTimeZone = "UTC"
 	}
 
 	bot, err := tgbotapi.NewBotAPI(token)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,7 +52,7 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://409f55f9ebfd.ngrok.io/" + bot.Token))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(localURL + bot.Token))
 
 	if err != nil {
 		log.Fatal(err)
@@ -40,7 +65,7 @@ func main() {
 	}
 
 	if info.LastErrorDate != 0 {
-		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+		log.Printf("Previous telegram callback failed: %s", info.LastErrorMessage)
 	}
 
 	updates := bot.ListenForWebhook("/" + bot.Token)
@@ -48,16 +73,24 @@ func main() {
 	go http.ListenAndServe(":8443", nil)
 
 	for update := range updates {
-		log.Printf("%+v\n", update)
-
 		if update.ChannelPost == nil {
 			continue
 		}
 
-		log.Printf("[%d] -- %+v", update.ChannelPost.Chat.ID, update.ChannelPost)
+		log.Printf("[%d] -- %s", update.ChannelPost.Chat.ID, update.ChannelPost.Text)
 
 		msg := tgbotapi.NewMessage(update.ChannelPost.Chat.ID, "Ahoy, mateys!")
 		msg.ReplyToMessageID = update.ChannelPost.MessageID
+
+		if update.ChannelPost.Text == "GO" {
+			created, err := createPoll()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Poll %s created with admin key %s", created.ID, created.AdminKey)
+		}
 
 		bot.Send(msg)
 	}
